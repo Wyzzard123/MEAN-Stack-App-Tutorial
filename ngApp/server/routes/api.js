de081d991e99585b114218adeebe70bbf1719d9a
Louiz Kim-Chan <kimblader151@yahoo.com.sg>
2020-08-12 11:56:20 +0800

    Added CRUD api for Express

--- ngApp/server/routes/api.js
+++ ngApp/server/routes/api.js
@@ -17,7 +17,6 @@ mongoose.Promise = global.Promise;
 mongoose.connect(db, function(err) {
   if (err) {
     console.error("Error! " + err);
-    console.log(mongo_db_user, mongo_db_password)
   }
 });
 
@@ -37,6 +36,69 @@ router.get('/videos', function(req, res){
         res.json(videos);
       }
     });
+});
+
+router.get('/video/:id', function(req, res){
+  console.log("Get request for a single video.")
+  // See this by going to http://localhost:3000/api
+  Video.findById(req.params.id)
+    .exec(function(err, video){
+      if (err) {
+        console.log("Error retrieving video: " + err)
+      } else {
+        res.json(video);
+      }
+    });
+});
+
+router.post('/video', function(req, res){
+  console.log("Post a video");
+  var newVideo = new Video();
+  newVideo.title = req.body.title;
+  newVideo.url = req.body.url;
+  newVideo.description =req.body.description;
+
+  // Inserts video into database
+  newVideo.save(function(err, insertedVideo) {
+    if(err) {
+      console.log("Error saving video:" + err);
+    } else {
+      res.json(insertedVideo);
+    }
+  })
+})
+
+router.put('/video/:id', function(req, res){
+  console.log("Update a video.");
+  Video.findByIdAndUpdate(req.params.id, {
+    $set: {
+      title: req.body.title,
+      url: req.body.url,
+      description: req.body.description
+    }
+  },
+  {
+      new: true
+  },
+  function(err, updatedVideo){
+    if(err) {
+      res.send("Error updating video");
+    } else {
+      res.json(updatedVideo);
+    }
+  }
+  )
+});
+
+router.delete('/video/:id', function(req, res) {
+  console.log("Deleting a video.");
+  Video.findByIdAndRemove(req.params.id, function(err, deletedVideo) {
+    if(err) {
+      res.send("Error deleting video: " + err);
+    } else {
+      res.json(deletedVideo);
+    }
+  });
 });
 
 module.exports = router;
